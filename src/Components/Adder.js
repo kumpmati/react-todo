4bbf9bb59c55ad102598d9869e6d26ba9e53e31d
Matias Kumpulainen <matias.v.kumpulainen@utu.fi>
2020-01-28 19:59:45 +0200

    App.js code refactor

--- src/Components/Adder.js
+++ src/Components/Adder.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+
+/**
+ * Adder is responsible for gathering user input
+ * and passing the TODO to the main app
+ */
 
 class Adder extends React.Component {
     render() {
         return (
             <div id="adder-wrapper">
-                <input type="text" placeholder="type your TODO here..." id="todo-text" />
-                <button onClick={() => 
-                    this.props.onClick(document.getElementById("todo-text").value)
-                } >Add item</button>
+                <input
+                    type="text"
+                    placeholder="type your TODO here..."
+                    id="todo-text"
+                />
+                <button
+                    onClick={() =>
+                        this.props.onClick(
+                            document.getElementById("todo-text").value
+                        )
+                    }
+                >
+                    Add item
+                </button>
             </div>
         );
     }
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import './App.css';
-import List from './List';
-import Adder from './Adder';
+import React from "react";
+import "./App.css";
+import List from "./List";
+import Adder from "./Adder";
 
 class App extends React.Component {
-
     constructor(props) {
         super(props);
-        this.state = {items: [], counter: 0};
+        this.state = { items: [], counter: 0 };
     }
 
     render() {
         return (
             <div id="wrapper">
-                <Adder onClick={e => this.addItem(e)}/>
-                <List itemList={this.state.items} handleDelete={e => this.deleteItem(e)} handleStatus={e => this.changeItem(e)}/>
+                <Adder onClick={e => this.addItem(e)} />
+                <List
+                    itemList={this.state.items}
+                    handleDelete={e => this.deleteItem(e)}
+                    handleStatus={e => this.changeItem(e)}
+                />
             </div>
         );
     }
@@ -24,30 +27,36 @@ class App extends React.Component {
             id: this.state.counter,
             text: item,
             date: new Date().toLocaleString(),
-            completed : false
+            completed: false
         };
         let newArr = this.state.items.slice();
         newArr.push(newItem);
-        this.setState({items: newArr, counter: this.state.counter + 1});
+        this.setState({ items: newArr, counter: this.state.counter + 1 });
     }
 
     deleteItem(itemToDelete) {
-        const newArr = this.state.items.slice();
-        const index = newArr.findIndex(element => element.id === itemToDelete.id);
-        if(index>-1) {
-            newArr.splice(index, 1);
-            this.setState({items: newArr});
+        const obj = this.findItemId(itemToDelete);
+        if (obj.i > -1) {
+            obj.arr.splice(obj.i, 1);
+            this.setState({ items: obj.arr });
         }
     }
 
     changeItem(itemToChange) {
-        const newArr = this.state.items.slice();
-        const index = newArr.findIndex(element => element.id === itemToChange.id);
-        if(index>-1) {
-            newArr[index].completed = !newArr[index].completed;
-            this.setState({items: newArr});
+        const obj = this.findItemId(itemToChange);
+        if (obj.i > -1) {
+            obj.arr[obj.i].completed = !obj.arr[obj.i].completed;
+            this.setState({ items: obj.arr });
         }
+    }
+
+    findItemId(item) {
+        const newArr = this.state.items.slice();
+        return {
+            i: newArr.findIndex(element => element.id === item.id),
+            arr: newArr
+        };
     }
 }
 
-export default App; 
+export default App;
