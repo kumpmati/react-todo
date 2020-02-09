6b00d31a4bb815731f397e7a764d31348e4398da
Matias Kumpulainen <matias.v.kumpulainen@utu.fi>
2020-02-09 14:57:30 +0200

    fixed item handling

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -8,7 +8,7 @@ class Item extends React.Component {
             <li key={this.props.id} className="item">
                 <h4 className="item-text">{this.props.text}</h4>
                 <p className="item-date">{this.props.date}</p>
-                <button onClick={() => this.props.handleDelete(this.props)}>Done</button>
+                <button onClick={() => this.props.handleItem("delete",this.props)}>Done</button>
             </li>
         );
     }
